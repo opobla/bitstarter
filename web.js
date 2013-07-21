6f734700425afa5384d9fec5279204f275732740
Oscar Garcia <oscarpoblacion@gmail.com>
2013-07-21 20:58:53 +0200

    web.js reads index.html

--- web.js
+++ web.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+b=fs.readFileSync("index.html");
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(b.toString());
 });
 
 var port = process.env.PORT || 5000;
